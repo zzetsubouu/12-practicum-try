df1882010702b710f2fb1e401b4527fc62a23116
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-11-19 21:43:31 +0200

    Update Program.cs

--- 12-practicum/Program.cs
+++ 12-practicum/Program.cs
@@ -9,6 +9,7 @@ namespace _12_practicum
     class Program
     {
         //Задача 1. Вычислить значение выражения, определив и использовав дополнительный метод:
+
         //static void Main(string[] args)
         //{
         //    double y = Func(6) + Func(13) + Func(21);
@@ -19,8 +20,19 @@ namespace _12_practicum
         //{
         //    return (Math.Sqrt(x) + x) / 2;
         //}
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Задача 2. Вычислить значение выражения, определив и использовав дополнительный метод:
         //static void Main(string[] args)
         //{
@@ -32,7 +44,20 @@ namespace _12_practicum
         //{
         //    return (Math.Sqrt(x1) + x1) / Math.Sqrt(x2) + x2;
         //}
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Задача 3. Найти максимальную величину из двух целых переменных a, b с использованием вспомогательных методов.
         //static void Main(string[] args)
         //{
@@ -45,10 +70,25 @@ namespace _12_practicum
         //}
         //static double Func(int a, int b)
         //{
-        //    //sdelat cherez if takzhe (vmesto math.max)
-        //    return Math.Max(a, b);
+        //    //вариант с тернарной операцией
+        //    int result = a>b ? a : b;
+        //    return result;
+        //    //2 вариант
+        //    //return Math.Max(a, b);
         //}
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Задача 4. Найти максимальную величину их трех целых переменных a, b, c с использованием вспомогательных методов. 
         //static void Main(string[] args)
         //{
@@ -58,18 +98,40 @@ namespace _12_practicum
         //    int b = int.Parse(Console.ReadLine());
         //    Console.Write("c=");
         //    int c = int.Parse(Console.ReadLine());
-        //    Console.WriteLine(Func(a, b,c));
-
+        //    Console.WriteLine(Func(a, b, c));
+        
         //    Console.ReadKey();
         //}
-        //static double Func(int a, int b,int c)
+        //static double Func(int a, int b, int c)
         //{
-        //    //sdelat cherez if takzhe (vmesto math.max)
-        //    return Math.Max(Math.Max(a, b),c);
+        //    //вариант с тернарной операцией
+        //    int result = a>c && a>b ? a : b>c &&b>a? b :c;
+        //    return result;
+        //    //2 вариант
+        //    //return Math.Max(Math.Max(a, b), c);
         //}
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Задача 5. Найти s=max(a, b) + max(c, d), используя вспомогательную функцию (метод).
+        
+        //вариант с 4 переменными
         //static void Main(string[] args)
         //{
         //    Console.Write("a=");
@@ -80,36 +142,73 @@ namespace _12_practicum
         //    int c = int.Parse(Console.ReadLine());
         //    Console.Write("d=");
         //    int d = int.Parse(Console.ReadLine());
-        //    Console.WriteLine($"s={Func(a, b, c,d)}");
-
+        //    Console.WriteLine($"s={Func(a, b, c, d)}");
+        
         //    Console.ReadKey();
         //}
         //static double Func(int a, int b, int c, int d)
         //{
-        //    //sdelat cherez if takzhe (vmesto math.max)
-        //    return Math.Max(a, b) + Math.Max(c, d);
+        //    int block1 = a>b?a:b;
+        //    int block2 = c>d?c:d;
+        //    return block1 + block2;
+        //    //2 вариант
+        //    //return Math.Max(a, b) + Math.Max(c, d);
         //}
-
+        
+        //вариант с 2 переменными
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("a=");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("b=");
+        //    int b = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"sum={Func(a, b) + Func(a, b)}");
+        
+        //    Console.ReadKey();
+        //}
+        //static double Func(int a, int b)
+        //{
+        //    int result = a > b ? a : b;
+        //    return result;
+        //}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //Задача 6. Вывести дни недели в зависимости от номера дня с использованием вспомогательного метода.
-        static void Main(string[] args)
-        {
-            DayOfWeek();
-            Console.ReadKey();
-        }
-        static void DayOfWeek()
-        {
-            Console.Write("введите номер дня недели: ");
-            int dayOfWeek = int.Parse(Console.ReadLine());
-            switch(dayOfWeek)
-            {
-                case 1: Console.WriteLine("понедельник"); break;
-                case 2: Console.WriteLine("вторник"); break;
-                case 3: Console.WriteLine("среда"); break;
-                case 4: Console.WriteLine("четверг"); break;
-                case 5: Console.WriteLine("пятница"); break;
-                case 6: Console.WriteLine("суббота"); break;
-                case 7: Console.WriteLine("воскресенье"); break;
-            }
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("введите номер дня недели: ");
+        //    int day = int.Parse(Console.ReadLine());
+        //    DayOfWeek(day);
+        //    Console.ReadKey();
+        //}
+        //static void DayOfWeek(int dayOfWeek)
+        //{
+        //    switch (dayOfWeek)
+        //    {
+        //        case 1: Console.WriteLine("понедельник"); break;
+        //        case 2: Console.WriteLine("вторник"); break;
+        //        case 3: Console.WriteLine("среда"); break;
+        //        case 4: Console.WriteLine("четверг"); break;
+        //        case 5: Console.WriteLine("пятница"); break;
+        //        case 6: Console.WriteLine("суббота"); break;
+        //        case 7: Console.WriteLine("воскресенье"); break;
+        //        default: Console.WriteLine("введите номер от 1 до 7"); break;
+        //    }
+        //}   
     }
 }
